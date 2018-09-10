27a05a15485bfad00fa5abbe4cb72f4e2d47c051
penglf121 <969330124@qq.com>
2018-09-10 23:16:45 +0800

    add quick

---
+++ spring-boot-quick/quick-activemq/src/main/java/com/mq/service/Consumer.java
@@ -0,0 +1,18 @@
+package com.mq.service;
+
+import com.alibaba.fastjson.JSON;
+import com.mq.model.TalentMQMessage;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+import javax.jms.TextMessage;
+
+@Component
+public class Consumer {
+
+    @JmsListener(destination = "${jsa.activemq.queue.name}",concurrency = "2-6")
+    public void receiveQueue1(TextMessage message) throws InterruptedException {
+        System.out.println("Consumer1收到的报文为: "+ message);
+    }
+
+}
